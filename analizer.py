68aeaecdb67380ea44b3ee05c885012470055eaf
Ionut Cicio <cicioionut328@gmail.com>
2019-12-04 19:47:50 +0100

    finally working

--- analizer.py
+++ analizer.py
@@ -6,9 +6,12 @@ input = open("input.java")
 informations = [Element("undefined", ["import"])]
 
 for line in input:
-    if re.search("^import .*;$", line):
-        print(str(re.findall(" .*;$", line)[0]))
+    if re.search("^import .*;$", line) != None:
         informations.append(Element(str(re.findall(" .*;$", line)[0]), ["import"]))
+    if re.search("public .*;$", line):
+        informations.append(Element(str(re.findall("c .*;$", line)[0]), ["public"]))
+    if re.search("private .*;$", line):
+        informations.append(Element(str(re.findall("e .*;$", line)[0]), ["private"]))
 
 def get_elements():
     return informations
