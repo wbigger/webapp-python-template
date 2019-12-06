18bbcc564f3e3b55c39da659876cbc177e05e0a7
Ionut Cicio <cicioionut328@gmail.com>
2019-12-06 17:34:54 +0100

    added search bar

--- static/script.js
+++ static/script.js
@@ -2,7 +2,23 @@ $.getJSON("/data").done(makeList);
 
 function makeList(jsonData) {
     for (element of jsonData) {
-        let item = `<li><div class="accessmodifier">${element.access_modifier}</div> <div class="keyword">${element.keyword}</div> <div class="datatype">${element.data_type}</div> <div class="identificator">${element.identifier}</div></li>`
-        $("ul").append(item);
+        let item = `<li><div class="accessmodifier">${element.access_modifier}</div> <div class="keyword">${element.keyword}</div> <div class="datatype">${element.data_type}</div> <div class="identifier">${element.identifier}</div></li>`
+        $("#list").append(item);
+    }
+}
+
+function sortList() {
+    $.getJSON("/data").done(makeSortedList);
+}
+
+function makeSortedList(jsonData){
+    $("#list").empty()
+    let word = $("#text").val()
+    for (element of jsonData) {
+        let elmentData = `${element.access_modifier} ${element.keyword} ${element.data_type} ${element.identifier}`
+        let item = `<li><div class="accessmodifier">${element.access_modifier}</div> <div class="keyword">${element.keyword}</div> <div class="datatype">${element.data_type}</div> <div class="identifier">${element.identifier}</div></li>`
+        if (elmentData.includes(word) || word == "") {
+            $("ul").append(item);
+        }
     }
 }
