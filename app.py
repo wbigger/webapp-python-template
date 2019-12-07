6823e7258782811240991d96e2a025d22a4f4cb8
daniele leo <danieleleo16@gmail.com>
2019-12-07 09:14:40 +0100

    Add SongList to the Server

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ from listamusica import songlist
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
+
 @app.route('/jquery-3.4.1.min.js')
 def js():
     return app.send_static_file('jquery-3.4.1.min.js')
+
 
 @app.route("/data")
 def data():
--- listamusica.py
+++ listamusica.py
@@ -1,14 +1,26 @@
 class Song:
-    def __init__(self, title, author, feat, album, length, year):
+    def __init__(self, title, author, feat, album, length, year, url):
         self.title = title
         self.author = author
         self.feat = feat
         self.album = album
         self.length = length
         self.year = year
+        self.url = url
 
-song_1 = Song("BASSOTTO", "DPG", "", "BASSOTTO", 1.58, 2019) 
-song_2 = Song("LIVIN' LA VIDA LOCA", "RICKY MARTIN", "", "", 4.03, 1999)
-song_3 = Song("SW1N6O", "Tha Supreme", "Salmo", "236451", 3.04, 2019)
 
-songlist = [song_1, song_2, song_3]
+song_1 = Song("Bassotto", "Tony Effe 777", "DPG", "BASSOTTO",
+              1.58, 2019, "https://youtu.be/Hv4GCqUgXUE")
+song_2 = Song("Livin' La Vida Loca", "Ricky Martin", "self",
+              "", 4.03, 1999, "https://youtu.be/p47fEXGabaY")
+song_3 = Song("SW1N6O", "Tha Supreme", "Salmo", "236451",
+              3.04, 2019, "https://youtu.be/HyazQiEEXO0")
+song_4 = Song("Monster Inc", "Disney", "self", "", 2.10,
+              2002, "https://youtu.be/aMwSNDRP90o")
+song_5 = Song("Giorno Theme RMX", "JoJo's Bizarre Adventure",
+              "self", "", 2.31, 2019, "https://youtu.be/EA9GnGwYTJg")
+song_6 = Song("L'Amour Toujours", "Gigi D'Agostino", "self",
+              "", 4.02, 1999, "https://youtu.be/w15oWDh02K4")
+song_7 = Song("Immigrato", "Checco Zalone", "self", "",
+              2.58, 2019, "https://youtu.be/SmH2ZZy0dUI")
+songlist = [song_1, song_2, song_3, song_4, song_5, song_6, song_7]
