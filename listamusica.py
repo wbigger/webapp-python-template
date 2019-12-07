840ea4a172444dc409d663cac209115ac87113f5
daniele leo <danieleleo16@gmail.com>
2019-12-07 09:32:33 +0100

    Add Songlist on the Playlist Server

--- listamusica.py
+++ listamusica.py
@@ -9,18 +9,18 @@ class Song:
         self.url = url
 
 
-song_1 = Song("Bassotto", "Tony Effe 777", "DPG", "BASSOTTO",
+song_1 = Song("Bassotto", "Tony Effe 777", "DPG", "Singolo",
               1.58, 2019, "https://youtu.be/Hv4GCqUgXUE")
 song_2 = Song("Livin' La Vida Loca", "Ricky Martin", "self",
-              "", 4.03, 1999, "https://youtu.be/p47fEXGabaY")
+              "Singolo", 4.03, 1999, "https://youtu.be/p47fEXGabaY")
 song_3 = Song("SW1N6O", "Tha Supreme", "Salmo", "236451",
               3.04, 2019, "https://youtu.be/HyazQiEEXO0")
-song_4 = Song("Monster Inc", "Disney", "self", "", 2.10,
+song_4 = Song("Monster Inc", "Disney", "self", "Disney Music", 2.10,
               2002, "https://youtu.be/aMwSNDRP90o")
 song_5 = Song("Giorno Theme RMX", "JoJo's Bizarre Adventure",
-              "self", "", 2.31, 2019, "https://youtu.be/EA9GnGwYTJg")
+              "self", "SoundTracks", 2.31, 2019, "https://youtu.be/EA9GnGwYTJg")
 song_6 = Song("L'Amour Toujours", "Gigi D'Agostino", "self",
-              "", 4.02, 1999, "https://youtu.be/w15oWDh02K4")
-song_7 = Song("Immigrato", "Checco Zalone", "self", "",
+              "Singolo", 4.02, 1999, "https://youtu.be/w15oWDh02K4")
+song_7 = Song("Immigrato", "Checco Zalone", "self", "Tolo Tolo",
               2.58, 2019, "https://youtu.be/SmH2ZZy0dUI")
 songlist = [song_1, song_2, song_3, song_4, song_5, song_6, song_7]
